fc76fdc595a9e4b85b529d921a890e6e045d9618
Rilysann <migornak@gmail.com>
2022-12-14 23:29:53 +0200

    Setup album slice

--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import photoSlice from './Photos/photoSlice';
+import albumSlice from './Albums/albumSlice';
 
 export const store = configureStore({
   reducer: {
     photos: photoSlice,
+    albums: albumSlice,
   },
 });
